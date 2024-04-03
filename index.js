e3efdf6d99db8bdeb1ca435a82e3e5085a2983c8
Mungai <mungaij697@gmail.com>
2024-04-03 08:46:41 +0300

    class functions

---
+++ index.js
@@ -0,0 +1,36 @@
+//const { returns } = require("chai-spies");
+
+//exerciseRoutine(function () {
+   // console.log("Stretch! Work that core!");
+  //});
+  
+  // "Go for a five-mile run"
+  // "Stretch! Work that core!"
+  function 
+  receivesAFunction(callback) {
+    console.log("callback function");
+   return callback();
+  }
+  function myCompany() {
+    
+    return callback();
+  }
+
+function returnsANamedFunction() {
+    function namedFunction(){
+        console.log("before all?");
+    }
+    return namedFunction
+}
+const namedFunction =
+returnsAnamedFunction();
+namedFunction();
+
+function returnsAnAnonymousFunction(){
+    return function() {
+        console.log("before all?");
+    };
+}
+const myAnonymousFunction = returnsAnAnonymousFunction();
+myAnonymousFunction();
+
